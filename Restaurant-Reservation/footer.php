aa562c1fb1556de6fdecfee138747bba6b17778c
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2024-11-10 12:18:16 +0530

    Add files via upload

---
+++ Restaurant-Reservation/footer.php
@@ -0,0 +1,23 @@
+<!--footer-->
+<footer class="footer" id="footer">
+ <div class="container"><br>
+  <div class="row">
+    <div class="col-sm-12 text-center">
+      <div class="social-icon">
+        <a href="index.php"><i class="fa fa-facebook"></i></a>
+        <a href="index.php"><i class="fa fa-twitter"></i></a>
+        <a href="index.php"><i class="fa fa-pinterest"></i></a>
+        <a href="index.php"><i class="fa fa-rss"></i></a>
+      </div>
+      <div class="copyright">
+        <p class="white"> copyright &copy; <b>Chocolate Divine</b></p>
+        <p><i class="fa fa-phone"></i> +91 7859567890 &nbsp;<i class="fa fa-envelope-o"></i> info@CohocolateDivine.com</p>
+      </div>
+    </div>
+  </div>
+ </div>
+</footer>
+
+</body>
+
+</html>
